005b693606c6627fc267c1294827f17023e78060
Mercy <aprilkasha42@gmail.com>
2020-05-02 12:36:55 +0300

    add changes to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
-(document).readyState(function()){
-    
+$(document).ready(function(){
+    $("#Birthday").submit(function(event)){
+        event.preventDefault();
+        var Birthday=$("input#Date").val();
+        var Gnder=$("select#Gender").val();
+    }
 }
+
+var days=
